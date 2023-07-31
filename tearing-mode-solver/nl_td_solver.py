27e1c5f1883a233ce89751055610c10ed5c40091
Marcus Quantrill <mjjq500@york.ac.uk>
2023-07-31 13:27:25 +0100

    Move to curve_fit

--- tearing-mode-solver/nl_td_solver.py
+++ tearing-mode-solver/nl_td_solver.py
@@ -10,6 +10,7 @@ from typing import Tuple
 from scipy.integrate import odeint
 from matplotlib import pyplot as plt
 from scipy.stats import sem
+from scipy.optimize import curve_fit
 
 # import warnings
 # warnings.filterwarnings("error")
@@ -109,6 +110,8 @@ def nl_tm_vs_time():
     )
     plt.show()
 
+def parabola(x, a, b, c):
+    return a*x**2 + b*x + c
 
 def nl_tm_small_w():
     m=2
@@ -117,7 +120,7 @@ def nl_tm_small_w():
     axis_q = 1.0
     solution_scale_factor = 1e-10
 
-    times = np.linspace(0.0, 1e1, 100)
+    times = np.linspace(0.0, 1e1, 100000)
 
     psi_t, w_t, tm0 = solve_time_dependent_system(
         m, n, resistivity, axis_q, solution_scale_factor, times
@@ -149,31 +152,46 @@ def nl_tm_small_w():
     #ax2.yaxis.label.set_color('red')
 
     # Returns highest power coefficients first
-    fit, cov = np.polyfit(times, np.squeeze(psi_t), 2, cov=True)
+    fit, cov = curve_fit(
+        parabola,
+        times,
+        psi_t,
+        p0=(a_theory, b_theory, c_theory),
+        bounds = ([0.0, 0.0, 0.0], [np.inf, np.inf, np.inf]),
+        x_scale = (1.0/psi0, 1.0/psi0, 1.0/psi0),
+        method='trf'
+    )
     perr = np.sqrt(np.diag(cov))
     print("Coefs: ", fit)
     print("Errors: ", perr)
     poly = np.poly1d(fit)
 
     ax.plot(
-        times, poly(times), label='Order 2 poly fit', linestyle='dashed',
+        times, parabola(times, *fit), label='Order 2 poly fit', linestyle='dashed',
         color='darkturquoise'
     )
 
-    poly_t = np.poly1d((a_theory, b_theory, c_theory))
+    t_fit = (a_theory, b_theory, c_theory)
     ax.plot(
-        times, poly_t(times), label='Theoretical poly', linestyle='dashed',
-        color='green'
+        times, parabola(times, *t_fit), label='Theoretical poly', linestyle='dotted',
+        color='red'
     )
     ax.set_xscale('log')
     ax.set_yscale('log')
 
     ax.legend(prop={'size': 7})
 
+    ls_data = np.sum((parabola(times, *fit) - psi_t)**2)
+    ls_theory = np.sum((parabola(times, *t_fit) - psi_t)**2)
+
+    print(ls_data, ls_theory)
+
     fig.tight_layout()
     #plt.show()
     savefig(f"nl_small_w_(m,n,A)=({m},{n},{solution_scale_factor})")
     plt.show()
+
+
 
 if __name__=='__main__':
     #nl_tm_vs_time()
