337639c5cf6384fafe8a62e1e7455760e5ebb79a
Marcus Quantrill <mjjq500@york.ac.uk>
2023-08-11 13:14:48 +0100

    Separate ql fitting from island width plot

--- tearing-mode-solver/quasi_linear_solver.py
+++ tearing-mode-solver/quasi_linear_solver.py
@@ -189,13 +189,13 @@ def nl_parabola(tm: TearingModeSolution,
     return c_0*(new_times**2) + c_1*new_times + c_2
 
 def ql_tm_vs_time():
-    m=3
-    n=2
+    m=2
+    n=1
     lundquist_number = 1e8
     axis_q = 1.0
     solution_scale_factor = 1e-10
 
-    times = np.linspace(0.0, 1e8, 300)
+    times = np.linspace(0.0, 1e8, 10000)
 
     psi_t, w_t, tm0, dps, ql_threshold, s = solve_time_dependent_system(
         m, n, lundquist_number, axis_q, solution_scale_factor, times
@@ -228,11 +228,73 @@ def ql_tm_vs_time():
 
     ax.plot(times, psi_t, label='Flux', color='black')
 
+    ax.set_xlabel(r"Normalised time ($\bar{\omega}_A t$)")
+    ax.set_ylabel(r"Normalised perturbed flux ($\delta \hat{\psi}^{(1)}$)")
+
+    ax2.plot(times, w_t, label='Normalised island width', color='red')
+    ax2.set_ylabel(r"Normalised layer width ($\hat{\delta}$)")
+    ax2.yaxis.label.set_color('red')
+    
+    ax.legend(prop={'size': 7}, loc='lower right')
+    
+    # ax.set_yscale('log')
+    # ax2.set_yscale('log')
+    ax.set_xscale('log')
+    ax2.set_xscale('log')
+    ax.set_yscale('log')
+    ax2.set_yscale('log')
+
+    fig.tight_layout()
+    #plt.show()
+    savefig(
+        f"ql_tm_time_evo_(m,n,A)=({m},{n},{solution_scale_factor})"
+    )
+    plt.show()
+
+def ql_with_fit_plots():
+    m=4
+    n=3
+    lundquist_number = 1e8
+    axis_q = 1.0
+    solution_scale_factor = 1e-10
+
+    times = np.linspace(0.0, 1e8, 10000)
+
+    psi_t, w_t, tm0, dps, ql_threshold, s = solve_time_dependent_system(
+        m, n, lundquist_number, axis_q, solution_scale_factor, times
+    )
+
+    lin_delta_prime, lin_growth_rate = growth_rate(
+        m,
+        n,
+        lundquist_number,
+        axis_q
+    )
+
+    #print("lgr: ", lin_growth_rate)
+    #print("Threshold: ", ql_threshold)
+    #print(psi_t)
+
+    fig, ax = plt.subplots(1, figsize=(4,3))
+
+    ql_time_min = time_from_flux(psi_t, times, 0.1*ql_threshold)
+    ql_time_max = time_from_flux(psi_t, times, 10.0*ql_threshold)
+
+    ax.fill_between(
+        [ql_time_min, ql_time_max],
+        2*[min(psi_t)],
+        2*[max(psi_t)],
+        alpha=0.3,
+        label='Quasi-linear region'
+    )
+
+    ax.plot(times, psi_t, label='Flux', color='black')
+
     lin_times = times[np.where(times < ql_time_max)]
     ax.plot(
         lin_times,
         psi_t[0]*np.exp(lin_growth_rate*lin_times),
-        label='Exponential time dependence (linear regime)'
+        label='Exponential fit'
     )
 
     nl_times = times[np.where(times >= ql_time_max)]
@@ -249,7 +311,7 @@ def ql_tm_vs_time():
             psi_t0_nl,
             nl_times
         ),
-        label="Quadratic time dependence (non-linear regime)"
+        label="Quadratic fit"
     )
 
 
@@ -257,25 +319,20 @@ def ql_tm_vs_time():
     ax.set_xlabel(r"Normalised time ($\bar{\omega}_A t$)")
     ax.set_ylabel(r"Normalised perturbed flux ($\delta \hat{\psi}^{(1)}$)")
 
-    ax2.plot(times, w_t, label='Normalised island width', color='red')
-    ax2.set_ylabel(r"Normalised layer width ($\hat{\delta}$)")
-    ax2.yaxis.label.set_color('red')
-    
-    ax.legend(prop={'size': 7})
-    
+    ax.legend(prop={'size': 7}, loc='lower right')
+
     # ax.set_yscale('log')
     # ax2.set_yscale('log')
     ax.set_xscale('log')
-    ax2.set_xscale('log')
     ax.set_yscale('log')
-    ax2.set_yscale('log')
 
     fig.tight_layout()
     #plt.show()
     savefig(
-        f"ql_tm_time_evo_(m,n,A)=({m},{n},{solution_scale_factor})"
+        f"ql_with_fitting_(m,n,A)=({m},{n},{solution_scale_factor})"
     )
     plt.show()
 
 if __name__=='__main__':
     ql_tm_vs_time()
+    #ql_with_fit_plots()
