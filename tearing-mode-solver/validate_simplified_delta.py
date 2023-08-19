e6d6de5bd310f192a3b4fdce01fd44d3f9bdad74
Marcus Quantrill <mjjq500@york.ac.uk>
2023-08-19 11:14:09 +0100

    Mods to plots

--- tearing-mode-solver/validate_simplified_delta.py
+++ tearing-mode-solver/validate_simplified_delta.py
@@ -5,23 +5,28 @@ from scipy.interpolate import UnivariateSpline
 from matplotlib import pyplot as plt
 from scipy.integrate import quad, simpson
 from tqdm import tqdm, trange
+from typing import Tuple
+import os
 
 from y_sol import Y
 from new_ql_solver import QuasiLinearSolution, nu, island_width
+from pyplot_helper import savefig
 
 def del_ql_full(sol: QuasiLinearSolution,
                 poloidal_mode: int,
                 toroidal_mode: int,
                 lundquist_number: float,
                 mag_shear: float,
-                r_s: float):
+                r_s: float,
+                x_range: Tuple[float, float],
+                dx: float = 0.1):
 
     times = sol.times
     w_t_func = UnivariateSpline(times, sol.w_t, s=0)
     dw_dt_func = w_t_func.derivative()
 
-
-    xs = np.linspace(-20.0, 20.0, 1000)
+    xmin, xmax = x_range
+    xs = np.arange(xmin, xmax, dx)
     ys = Y(xs)
     ys_func = UnivariateSpline(xs, ys, s=0)
     d2ydx2 = ys_func.derivative().derivative()
@@ -99,13 +104,84 @@ def classFromArgs(className, df):
                        if col in fieldSet}
     return className(**filteredArgDict)
 
-if __name__=='__main__':
+def convergence_of_delta_prime():
     m=2
     n=1
     S=1e8
     s=5.84863459819362
     r_s=0.7962252761034401
-    
+
+    fname = "./output/18-08-2023_16:41_new_ql_tm_time_evo_(m,n,A)=(2,1,1e-10).csv"
+    df = pd.read_csv(fname)
+    ql_sol = classFromArgs(QuasiLinearSolution, df)
+
+    delta_ql_orig = island_width(
+        ql_sol.psi_t,
+        ql_sol.dpsi_dt,
+        ql_sol.d2psi_dt2,
+        r_s,
+        m,
+        n,
+        s,
+        S
+    )
+
+    simple_integration()
+
+    x_lims = [1.0, 1.5, 20.0]
+
+
+    fig_dp, ax_dp = plt.subplots(1, figsize=(4,4))
+    ax_dp.plot(
+        ql_sol.times,
+        ql_sol.delta_primes,
+        label=r"Approximate $a\Delta'$",
+        color='black'
+    )
+    ax_dp.set_xscale('log')
+
+    for xlim in x_lims:
+        delqls, times, xs = del_ql_full(
+            ql_sol, m, n, S, s, r_s,
+            (-xlim, xlim),
+            0.1
+        )
+        #delqls = delqls[:,::1000]
+
+        delta_primes = delta_prime_full(
+            delqls,
+            xs,
+            times,
+            ql_sol.psi_t,
+            ql_sol.dpsi_dt,
+            r_s,
+            S
+        )
+
+        times_f = times[times>1e3]
+        delta_primes_f = delta_primes[-len(times_f):]
+        ax_dp.plot(
+            times_f, delta_primes_f, label=r"Exact $a\Delta'$"+f"(X={xlim})"
+        )
+
+    ax_dp.legend()
+    ax_dp.set_xlabel(r"Normalised time $\bar{\omega}_A t$")
+    ax_dp.set_ylabel(r"$a\Delta'$")
+    ax_dp.set_xlim(left=1e3)
+    #ax_dp.set_ylim(bottom=1.0)
+    fig_dp.tight_layout()
+
+    orig_fname, ext = os.path.splitext(os.path.basename(fname))
+    savefig(f"{orig_fname}_delta_prime_convergence")
+
+
+def convergence_of_growth_rate():
+    m=2
+    n=1
+    S=1e8
+    s=5.84863459819362
+    r_s=0.7962252761034401
+
     fname = "./output/18-08-2023_16:41_new_ql_tm_time_evo_(m,n,A)=(2,1,1e-10).csv"
     df = pd.read_csv(fname)
     ql_sol = classFromArgs(QuasiLinearSolution, df)
@@ -125,52 +201,57 @@ if __name__=='__main__':
 
     delqls, times, xs = del_ql_full(ql_sol, m, n, S, s, r_s)
     #delqls = delqls[:,::1000]
-    
-    # delta_primes = delta_prime_full(
-    #     delqls,
-    #     xs,
-    #     times,
-    #     ql_sol.psi_t,
-    #     ql_sol.dpsi_dt,
-    #     r_s,
-    #     S
-    # )
-    
-    # fig_dp, ax_dp = plt.subplots(1, figsize=(4,4))
-    # ax_dp.plot(times, delta_primes, label=r"Exact $\Delta'$")
-    # ax_dp.plot(times, ql_sol.delta_primes, label=r"Approximate $\Delta'$")
-    
+
+    delta_primes = delta_prime_full(
+        delqls,
+        xs,
+        times,
+        ql_sol.psi_t,
+        ql_sol.dpsi_dt,
+        r_s,
+        S,
+        (-10.0, 10.0),
+        0.1
+    )
+
+    fig_dp, ax_dp = plt.subplots(1, figsize=(4,4))
+    ax_dp.plot(times, delta_primes, label=r"Exact $\Delta'$")
+    ax_dp.plot(times, ql_sol.delta_primes, label=r"Approximate $\Delta'$")
+
     # fig, ax = plt.subplots(1, figsize=(4,4))
     # plt.imshow(
     #     delqls,
     #     extent=[min(times), max(times), min(xs), max(xs)]
     # )
-    
+
     # ax.set_aspect((max(times)-min(times))/(max(xs)-min(xs)))
-    
+
     fig2, ax2 = plt.subplots(1, figsize=(4,3))
-    
+
     ax2.plot(times, delta_ql_orig, label=f'Recreated approximate soln')
-    ax2.plot(times, ql_sol.w_t, label=f'Approximate solution') 
+    ax2.plot(times, ql_sol.w_t, label=f'Approximate solution')
     ax2.plot(times, delqls[-1,:], label=f'x={xs[-1]:.2f}')
     ax2.set_xscale('log')
     ax2.set_yscale('log')
-    
+
     for i in range(len(xs)//2, len(xs), 200):
         lineout = delqls[i,:]
         ax2.plot(times, lineout, label=f'x={xs[i]:.2f}')
-        
-        
+
+
     ax2.legend()
-    
+
     fig_psi, ax_psi = plt.subplots(1)
     ax_psi.plot(times, ql_sol.psi_t)
     ax_w = ax_psi.twinx()
-    
+
     ax_psi.set_xscale('log')
     ax_psi.set_yscale('log')
-    
+
     ax_w.plot(times, ql_sol.w_t)
     ax_w.set_yscale('log')
+
+if __name__=='__main__':
+    convergence_of_delta_prime()
         
     plt.show()
