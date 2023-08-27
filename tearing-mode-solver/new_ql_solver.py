a645c3dc500a2b49f715d389a84a68277e5162b5
Marcus Quantrill <mjjq500@york.ac.uk>
2023-08-27 17:32:08 +0100

    Comment new ql solver

--- tearing-mode-solver/new_ql_solver.py
+++ tearing-mode-solver/new_ql_solver.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm, trange
 import pandas as pd
 from dataclasses import dataclass, asdict
+from typing import Tuple
 
 from linear_solver import (
     TearingModeSolution,
@@ -23,9 +24,23 @@ from helpers import savefig, savecsv, TimeDependentSolution
 
 @np.vectorize
 def nu(psi_rs: float,
-       poloidal_mode: float,
+       poloidal_mode: int,
        lundquist_number: float,
        r_s: float) -> float:
+    """
+    Calculate the \nu parameter to be used in the calculation for the
+    quasi-linear layer width.
+
+    Parameters:
+        psi_rs: float
+            The perturbed flux at the resonant surface
+        poloidal_mode: int
+            Poloidal mode of the tearing mode
+        lundquist_number: float
+            The Lundquist number
+        r_s: float
+            Location of the resonant surface (normalised to minor radius)
+    """
     S = lundquist_number
     m = poloidal_mode
 
@@ -36,10 +51,32 @@ def island_width(psi_rs: float,
                  dpsi_dt: float,
                  d2psi_dt2: float,
                  r_s: float,
-                 poloidal_mode: float,
-                 toroidal_mode: float,
+                 poloidal_mode: int,
+                 toroidal_mode: int,
                  mag_shear: float,
                  lundquist_number: float) -> float:
+    """
+    Calculate the quasi-linear island width of the tearing mode.
+
+    Parameters:
+        psi_rs: float
+            Perturbed flux at the resonant surface
+        dpsi_dt: float
+            First derivative in perturbed flux at the resonant surface
+        d2psi_dt2: float
+            Second derivative in perturbed flux at the resonant surface
+        r_s: float
+            Location of the resonant surface normalised to plasma minor radius
+        poloidal_mode: int
+            Poloidal mode number of the tearing mode
+        toroidal_mode: int
+            Toroidal mode number of the tearing mode
+        mag_shear: float
+            Magnetic shear at the resonant surface. See magnetic_shear() in
+            linear_solver.py
+        lundquist_number: float
+            The Lundquist number
+    """
     S = lundquist_number
     n = toroidal_mode
     m = poloidal_mode
@@ -53,6 +90,8 @@ def island_width(psi_rs: float,
 
     pre_factor = (non_linear_term + linear_term)
 
+    # Avoid complex results by checking if argument is greater than zero
+    # before fourth-rooting it.
     if pre_factor >= 0.0:
         return (pre_factor)**(1/4)/denominator
 
@@ -63,7 +102,26 @@ def quasi_linear_threshold(toroidal_mode: int,
                            r_s: float,
                            mag_shear: float,
                            lundquist_number: float,
-                           delta_prime_linear: float):
+                           delta_prime_linear: float) -> float:
+    """
+    Calculate the threshold perturbed flux needed for a tearing mode to be
+    considered to be in the quasi-linear regime.
+
+    Parameters:
+        toroidal_mode: int
+            Toroidal mode number of the tearing mode
+        r_s: float
+            Location of the resonant surface normalised to the plasma minor
+            radius
+        mag_shear: float
+            Magnetic shear at the resonant surface. See magnetic_shear() in
+            linear_solver.py
+        lundquist_number: float
+            The Lundquist number
+        delta_prime_linear: float
+            Discontinuity parameter calculated in the linear regime. See
+            delta_prime() in linear_solver.py
+    """
     g = gamma_constant()
     n = toroidal_mode
     s = mag_shear
@@ -75,19 +133,46 @@ def quasi_linear_threshold(toroidal_mode: int,
 #ode_island_width = []
 
 def flux_time_derivative(time: float,
-                         var: float,
+                         var: Tuple[float, float],
                          tm: TearingModeSolution,
                          poloidal_mode: int,
                          toroidal_mode: int,
                          lundquist_number: float,
                          mag_shear: float,
                          init_island_width: float,
-                         delta_prime: float,
-                         epsilon: float = 1e-5):
+                         delta_prime: float):
+    """
+    Calculate first and second order time derivatives of the perturbed flux
+    in the inner region of the tearing mode using the quasi-linear time
+    evolution equations (delta model).
+
+    This is passed to scipy's ODE function to be integrated.
+
+    Parameters:
+        time: float
+            The current time of the simulation.
+        var: Tuple[float, float]
+            Tuple containing the perturbed flux and rate of change of perturbed
+            flux at the current time
+        tm: TearingModeSolution
+            The outer solution of the current tearing mode
+        poloidal_mode: int
+            Poloidal mode number of the tearing mode
+        toroidal_mode: int
+            Toroidal mode number of the tearing mode
+        lundquist_number: float
+            The Lundquist number
+        mag_shear: float
+            Magnetic shear at the resonant surface. See magnetic_shear() in
+            linear_solver.py
+        init_island_width: float
+            Quasi-linear island width at the current time.
+        delta_prime: float
+            The non-linear discontinuity parameter at the current time.
+    """
 
     psi, dpsi_dt = var
 
-    #global ode_island_width
     w = init_island_width
 
     m = poloidal_mode
@@ -107,22 +192,6 @@ def flux_time_derivative(time: float,
 
     d2psi_dt2 = dpsi_dt * (linear_term + non_linear_term)
 
-    #w_new = island_width(
-        #psi,
-        #dpsi_dt,
-        #d2psi_dt2,
-        #tm.r_s,
-        #m,
-        #n,
-        #s,
-        #S
-    #)
-
-    #print(w, dpsi_dt, d2psi_dt2)
-
-    #ode_island_width.append(w_new)
-    
-    #print(f"iter time={time}")
 
     return [dpsi_dt, d2psi_dt2]
 
@@ -135,11 +204,35 @@ def solve_time_dependent_system(poloidal_mode: int,
                                 initial_scale_factor: float = 1.0,
                                 t_range: np.array = np.linspace(0.0, 1e5, 10))\
                                     -> TimeDependentSolution:
+    """
+    Numerically integrate the quasi-linear flux time derivative of a tearing
+    mode.
+
+    Parameters:
+        poloidal_mode: int
+                Poloidal mode number of the tearing mode
+        toroidal_mode: int
+            Toroidal mode number of the tearing mode
+        lundquist_number: float
+            The Lundquist number
+        axis_q: float
+            The on-axis equilibrium safety factor
+        initial_scale_factor: float
+            The value of the perturbed flux at the resonant surface at t=0
+        t_range: np.array
+            Array of time values to record. Each element will have an associated
+            perturbed flux, derivative etc calculated for that time.
+    """
 
     tm = solve_system(poloidal_mode, toroidal_mode, axis_q)
     #tm_s = scale_tm_solution(tm, initial_scale_factor)
 
     psi_t0 = initial_scale_factor#tm.psi_forwards[-1]
+
+    # Calculate the initial growth rate of the mode using the linear theory
+    # result. We hence assume that we are solving this system with an initially
+    # small perturbed flux. If the flux were large, then this growth rate
+    # would not be valid and an alternative growth rate must be used.
     delta_prime, linear_growth_rate = growth_rate(
         poloidal_mode,
         toroidal_mode,
@@ -148,6 +241,8 @@ def solve_time_dependent_system(poloidal_mode: int,
     )
     dpsi_dt_t0 = linear_growth_rate * psi_t0
 
+    # Calculate the initial width of the magnetic island using the linear layer
+    # width.
     init_island_width = layer_width(
         poloidal_mode,
         toroidal_mode,
@@ -161,6 +256,7 @@ def solve_time_dependent_system(poloidal_mode: int,
     t0 = t_range[0]
     tf = t_range[-1]
     dt = t_range[1]-t_range[0]
+    # Use the vode algorithm provided by ODE. This is more stable than lsoda.
     r = ode(flux_time_derivative).set_integrator(
         'vode', method='bdf'
     )
@@ -175,6 +271,7 @@ def solve_time_dependent_system(poloidal_mode: int,
         delta_prime
     )
 
+    # Set up return parameters.
     w_t = [init_island_width]
     psi = [psi_t0]
     dpsi_dt = [dpsi_dt_t0]
@@ -182,7 +279,9 @@ def solve_time_dependent_system(poloidal_mode: int,
     delta_primes = [delta_prime]
     times = [0.0]
 
-    print(dt)
+    # Loop through time range and integrate after each time. The island width
+    # is updated outside of the derivative function as it relies on the
+    # first and second time derivatives from the previous iteration.
     tqdm_range = trange(len(t_range)-1, desc='Time: ', leave=True)
     for i in tqdm_range:
         if not r.successful():
@@ -192,6 +291,8 @@ def solve_time_dependent_system(poloidal_mode: int,
         tqdm_range.set_description(f"Time: {r.t:.2f}", refresh=True)
         
         #print(r.t, dt)
+
+        # Integrate then calculate derivatives.
         psi_now, dpsi_dt_now = r.integrate(r.t+dt)
 
         _, d2psi_dt2_now = flux_time_derivative(
@@ -212,6 +313,7 @@ def solve_time_dependent_system(poloidal_mode: int,
         w_t.append(init_island_width)
         d2psi_dt2.append(d2psi_dt2_now)
 
+        # Use the derivatives to calculate new island width.
         init_island_width = island_width(
             psi_now,
             dpsi_dt_now,
@@ -237,27 +339,6 @@ def solve_time_dependent_system(poloidal_mode: int,
             delta_prime
         )
 
-    #result = odeint(
-        #flux_time_derivative,
-        #(psi_t0, dpsi_dt_t0),
-        #t_range,
-        #args = (
-            #tm,
-            #poloidal_mode,
-            #toroidal_mode,
-            #lundquist_number,
-            #s
-        #)
-    #)
-
-
-    # We get weird numerical bugs sometimes returning large or nan values.
-    # Set these to zero.
-    #psi_t[np.abs(psi_t) > 1e10] = 0.0
-    #psi_t[np.argwhere(np.isnan(psi_t))] = 0.0
-
-    #dps = [delta_prime_non_linear(tm, w) for w in w_t]
-
     return TimeDependentSolution(
         np.squeeze(times),
         np.squeeze(psi),
@@ -270,19 +351,27 @@ def solve_time_dependent_system(poloidal_mode: int,
 def time_from_flux(psi: np.array,
                    times: np.array,
                    target_psi: float):
+    """
+    Given an array of perturbed fluxes and times, find the closest time
+    associated with the target flux (target_psi).
+    """
     min_index = np.abs(psi - target_psi).argmin()
     return times[min_index]
 
 
 
 def ql_tm_vs_time():
+    """
+    Numerically solve the quasi-linear time-dependent tearing mode problem
+    and plot the perturbed flux and layer width as functions of time.
+    """
     m=2
     n=1
     lundquist_number = 1e8
-    axis_q = 1.2
+    axis_q = 1.0
     solution_scale_factor = 1e-10
 
-    times = np.linspace(0.0, 1e8, 100000)
+    times = np.linspace(0.0, 1e6, 1000)
 
     ql_solution = solve_time_dependent_system(
         m, n, lundquist_number, axis_q, solution_scale_factor, times
@@ -330,6 +419,9 @@ def ql_tm_vs_time():
     plt.show()
 
 def ql_with_fit_plots():
+    """
+    Deprecated function.
+    """
     m=4
     n=3
     lundquist_number = 1e8
