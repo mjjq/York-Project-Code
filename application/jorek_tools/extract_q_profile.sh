5512086df6f7e7193c24f9f38857ed83b782f5f2
Marcus Quantrill <ir-quan1@login-q-1.data.cluster>
2024-04-25 14:01:57 +0100

    change extraction script locations

--- application/jorek_tools/extract_q_profile.sh
+++ application/jorek_tools/extract_q_profile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./jorek2_postproc < $JOREK_ANALYSIS/extraction_scripts/get_flux.pp
+./jorek2_postproc < $JOREK_TOOLS/get_flux.pp
 
-python3 $JOREK_ANALYSIS/extraction_scripts/jorek_dat_to_array.py exprs_averaged_s00000.dat qprofile_s00000.dat
+python3 $JOREK_TOOLS/jorek_dat_to_array.py exprs_averaged_s00000.dat qprofile_s00000.dat
