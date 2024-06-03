c4b0f658bd0c7450f22cd3996ad21a8eb72fd8cc
Marcus Quantrill <marcus_quantrill@hotmail.com>
2024-06-03 10:40:30 +0100

    Document q profile extraction script

--- application/jorek_tools/extract_q_profile.sh
+++ application/jorek_tools/extract_q_profile.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# USAGE: cd to root directory of a jorek simulation then run this script
+# This script uses postproc to extract the qprofile and current profile,
+# mapping it from normalised psi to the radial co-ordinate r. This is used
+# in model_jorek_params.py inside the experiments subfolder of the tearing
+# mode solver code.
+
 ./jorek2_postproc < $JOREK_TOOLS/get_flux.pp
 
 cd postproc
