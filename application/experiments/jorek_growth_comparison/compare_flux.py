ad8a458b0af00a3dd93b381044903082f46f4a69
mjjq <marcus_quantrill@hotmail.com>
2024-06-05 18:22:20 +0100

    Update model params and flux comparison scripts

--- application/experiments/jorek_growth_comparison/compare_flux.py
+++ application/experiments/jorek_growth_comparison/compare_flux.py
@@ -6,18 +6,21 @@ import numpy as np
 from typing import List, Tuple
 
 import imports
-from tearing_mode_solver.outer_region_solver import (
-    rational_surface, magnetic_shear
-)
+from tearing_mode_solver.outer_region_solver import rational_surface, magnetic_shear
 from tearing_mode_solver.delta_model_solver import nu, mode_width
 from tearing_mode_solver.helpers import (
-    classFromArgs, TimeDependentSolution, savefig, load_sim_from_disk
+    classFromArgs,
+    TimeDependentSolution,
+    savefig,
+    load_sim_from_disk,
 )
 from tearing_mode_solver.outer_region_solver import island_width
 from tearing_mode_solver.algebraic_fitting import get_parab_coefs
 from jorek_tools.calc_jorek_growth import growth_rate, _name_time, _name_flux
 from jorek_tools.jorek_dat_to_array import q_and_j_from_csv
 from jorek_tools.psi_t_from_vtk import jorek_flux_at_q
+from jorek_tools.time_conversion import jorek_to_alfven_time
+
 
 def check_model_t_dependence():
     model_data_filename = "./output/15-05-2024_16:49_jorek_model_(m,n)=(2,1).zip"
@@ -36,24 +39,21 @@ def check_model_t_dependence():
     fig, ax = plt.subplots(1)
 
     ax.plot(times, psi_t)
-    ax.set_xscale('log')
+    ax.set_xscale("log")
 
     plt.show()
-   
+
 
 def ql_tm_vs_time():
     """
     Plot various numerically solved variables from a tearing mode solution and
     island width as a function of time from .csv data.
     """
-    model_data_filename = "./output/04-06-2024_14:27_jorek_model_(m,n)=(2,1).zip"
+    model_data_filename = "./output/05-06-2024_16:42_jorek_model_(m,n)=(2,1).zip"
     jorek_data_filename = "../../jorek_tools/postproc/psi_t_data.csv"
     q_prof_filename = "../../jorek_tools/postproc/qprofile_s00000.dat"
     psi_current_prof_filename = "../../jorek_tools/postproc/exprs_averaged_s00000.csv"
-    q_profile, j_profile = q_and_j_from_csv(
-        psi_current_prof_filename, q_prof_filename
-    )
-
+    q_profile, j_profile = q_and_j_from_csv(psi_current_prof_filename, q_prof_filename)
 
     params, sol = load_sim_from_disk(model_data_filename)
 
@@ -64,100 +64,92 @@ def ql_tm_vs_time():
     d2psi_dt2 = sol.d2psi_dt2
     delta_primes = sol.delta_primes
 
-
     jorek_data = pd.read_csv(jorek_data_filename).fillna(0)
-    jorek_times, jorek_flux = jorek_flux_at_q(jorek_data, q_profile, 2/1)
-    
+    jorek_times, jorek_flux = jorek_flux_at_q(jorek_data, q_profile, 2 / 1)
+    jorek_times = jorek_to_alfven_time(jorek_times, params.B0, params.R0)
 
-    min_time = 0.0#1e4
-    max_time = 1e6
+    min_time = np.min(times)  # 1e4
+    max_time = np.max(times)
 
     # Model flux
-    model_filt = ((times<max_time) & (times> min_time))
+    model_filt = (times < max_time) & (times > min_time)
     times = times[model_filt]
     model_flux = psi_t[model_filt]
     model_flux_func = UnivariateSpline(times, model_flux, s=0)
 
     # JOREK flux
-    jorek_filt = jorek_times>min_time
+    jorek_filt = jorek_times > min_time
     jorek_times = jorek_times[jorek_filt]
     jorek_flux = jorek_flux[jorek_filt]
 
     # Parabolic fitting
-    min_t2_time = 1.9e5
-    max_t2_time = 5e5
-    c_0, c_1, c_2 = get_parab_coefs(params, model_flux_func(min_t2_time))
+    # min_t2_time = 1.9e5
+    # max_t2_time = 5e5
+    # c_0, c_1, c_2 = get_parab_coefs(params, model_flux_func(min_t2_time))
 
-    t = np.linspace(min_t2_time, max_t2_time, 100)
+    # t = np.linspace(min_t2_time, max_t2_time, 100)
 
-    fig, ax = plt.subplots(1, figsize=(5,4))
+    fig, ax = plt.subplots(1, figsize=(5, 4))
 
-    ax.plot(times, model_flux, label='Model', color='black')
-    ax.plot(jorek_times, jorek_flux, label='JOREK', color='red')
+    ax.plot(times, model_flux, label="Model", color="black")
+    ax.plot(jorek_times, jorek_flux, label="JOREK", color="red")
 
-    log_times = np.logspace(4, np.log(max(times)), 100)
-    #ax.plot(log_times, 1.0/log_times, label='2/t dependence')
+    #log_times = np.logspace(4, np.log(max(times)), 100)
+    ## ax.plot(log_times, 1.0/log_times, label='2/t dependence')
 
+    ax.set_xscale("log")
+    ax.set_yscale("log")
 
-    ax.set_xscale('log')
-    ax.set_yscale('log')
+    #ax.set_xlabel(r"Time ($1/\omega_A$)")
+    #ax.set_ylabel(r"Flux at rational surface ($a^2 B_{\phi 0}$)")
 
-    ax.set_xlabel(r"Time ($1/\omega_A$)")
-    ax.set_ylabel(r"Flux at rational surface ($a^2 B_{\phi 0}$)")
-
-
-
-    ax.plot(
-        t, 
-        c_0*(t-min_t2_time)**2 +c_1*(t-min_t2_time) + c_2, 
-        color='green', linestyle='--', 
-        label=r'$f(t) = at^2 + bt + c$' + \
-            f"\n a={c_0:.2e},\n b={c_1:.2e},\n c={c_2:.2e}"
-    )
+    ## ax.plot(
+    ##     t,
+    ##     c_0*(t-min_t2_time)**2 +c_1*(t-min_t2_time) + c_2,
+    ##     color='green', linestyle='--',
+    ##     label=r'$f(t) = at^2 + bt + c$' + \
+    ##         f"\n a={c_0:.2e},\n b={c_1:.2e},\n c={c_2:.2e}"
+    ## )
 
     ax.legend()
     fig.tight_layout()
-    
-    savefig("flux_comparison_log_log")
-    
-    ax.set_xlim(left=40000, right=1e6)
-    ax.set_ylim(bottom=1e-9, top=1e-2)
-    fig.tight_layout()
-    
-    savefig("flux_comparison_log_log_zoom")
-    
-    ax.set_xscale('linear')
-    ax.set_yscale('linear')
-    fig.set_size_inches(6, 4.65, forward=True)
-    ax.set_xlim(left=0, right=4e5)
-    ax.set_ylim(bottom=0, top=0.00175)
-    fig.tight_layout()
-    
-    savefig("flux_comparison_lin_lin_early")
-    
-    ax.set_xlim(left=0, right=1e6)
-    ax.set_ylim(bottom=0, top=0.00175)
-    
-    savefig("flux_comparison_lin_lin")
-    
-    
-    ax.autoscale()
-    ax.set_yscale("log")
-    ax.set_xlim(left=0, right=5e5)
-    savefig("flux_comparison_lin_log")
-    
-    ax.set_xscale("linear")
-    ax.set_yscale("linear")
-    ax.set_xlim(left=0, right=8e4)
-    ax.set_ylim(bottom=0, top=1e-5)
-    savefig("flux_comparison_linear_regime")
+
+    #savefig("flux_comparison_log_log")
+
+    #ax.set_xlim(left=40000, right=1e6)
+    #ax.set_ylim(bottom=1e-9, top=1e-2)
+    #fig.tight_layout()
+
+    #savefig("flux_comparison_log_log_zoom")
+
+    #ax.set_xscale("linear")
+    #ax.set_yscale("linear")
+    #fig.set_size_inches(6, 4.65, forward=True)
+    #ax.set_xlim(left=0, right=4e5)
+    #ax.set_ylim(bottom=0, top=0.00175)
+    #fig.tight_layout()
+
+    #savefig("flux_comparison_lin_lin_early")
+
+    #ax.set_xlim(left=0, right=1e6)
+    #ax.set_ylim(bottom=0, top=0.00175)
+
+    #savefig("flux_comparison_lin_lin")
+
+    #ax.autoscale()
+    #ax.set_yscale("log")
+    #ax.set_xlim(left=0, right=5e5)
+    #savefig("flux_comparison_lin_log")
+
+    #ax.set_xscale("linear")
+    #ax.set_yscale("linear")
+    #ax.set_xlim(left=0, right=8e4)
+    #ax.set_ylim(bottom=0, top=1e-5)
+    #savefig("flux_comparison_linear_regime")
 
     plt.show()
 
 
-
-
-
-if __name__=='__main__':
+if __name__ == "__main__":
     ql_tm_vs_time()
-    #check_model_t_dependence()
+    # check_model_t_dependence()
--- application/experiments/jorek_growth_comparison/model_jorek_params.py
+++ application/experiments/jorek_growth_comparison/model_jorek_params.py
@@ -7,41 +7,45 @@ import imports
 
 from jorek_tools.jorek_dat_to_array import q_and_j_from_csv
 from tearing_mode_solver.delta_model_solver import solve_time_dependent_system
-from tearing_mode_solver.outer_region_solver import \
-    solve_system, OuterRegionSolution, normalised_energy_integral, energy
+from tearing_mode_solver.outer_region_solver import (
+    solve_system,
+    OuterRegionSolution,
+    normalised_energy_integral,
+    energy,
+)
 from tearing_mode_solver.helpers import (
-    savefig, 
+    savefig,
     TearingModeParameters,
     sim_to_disk,
-    TimeDependentSolution
+    TimeDependentSolution,
 )
 from jorek_tools.time_conversion import jorek_to_alfven_time
 
 def plot_growth(times, dpsi_t, psi_t):
-    fig_growth, ax_growth = plt.subplots(1, figsize=(4.5,3))
+    fig_growth, ax_growth = plt.subplots(1, figsize=(4.5, 3))
 
-    ax_growth.plot(times, dpsi_t/psi_t, color='black')
-    ax_growth.set_ylabel(r'Growth rate $\delta\dot{\psi}^{(1)}/\delta\psi^{(1)}$')
-    ax_growth.set_xlabel(r'Normalised time $1/\bar{\omega}_A$')
+    ax_growth.plot(times, dpsi_t / psi_t, color="black")
+    ax_growth.set_ylabel(r"Growth rate $\delta\dot{\psi}^{(1)}/\delta\psi^{(1)}$")
+    ax_growth.set_xlabel(r"Normalised time $1/\bar{\omega}_A$")
 
-    plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+    plt.ticklabel_format(style="sci", axis="x", scilimits=(0, 0))
 
     fig_growth.tight_layout()
 
-    ax_growth.set_xscale('log')
-    ax_growth.set_yscale('log')
+    ax_growth.set_xscale("log")
+    ax_growth.set_yscale("log")
 
-    ax_growth.plot(times, 2/times, color='red', label='2/t dependence')
-    #orig_fname, ext = os.path.splitext(os.path.basename(model_data_filename))
-    #savefig("jorek_model_growth_rate")
+    ax_growth.plot(times, 2 / times, color="red", label="2/t dependence")
+    # orig_fname, ext = os.path.splitext(os.path.basename(model_data_filename))
+    # savefig("jorek_model_growth_rate")
 
-def plot_energy(params: TearingModeParameters,
-                td_sol: TimeDependentSolution,
-                outer_sol: OuterRegionSolution):
-    norm_energy = normalised_energy_integral(
-        outer_sol,
-        params
-    )
+
+def plot_energy(
+    params: TearingModeParameters,
+    td_sol: TimeDependentSolution,
+    outer_sol: OuterRegionSolution,
+):
+    norm_energy = normalised_energy_integral(outer_sol, params)
 
     print(f"Normalised energy integral: {norm_energy}")
 
@@ -49,67 +53,65 @@ def plot_energy(params: TearingModeParameters,
 
     fig, axs = plt.subplots(2)
 
-    ax,ax2 = axs
+    ax, ax2 = axs
 
     ax.plot(td_sol.times, energies)
 
-    ax.set_yscale('log')
+    ax.set_yscale("log")
     ax.set_xlabel("Time ($1/\omega_A$)")
     ax.set_ylabel("Energy")
 
     ax2.plot(outer_sol.r_range_fwd, outer_sol.psi_forwards)
     ax2.plot(outer_sol.r_range_bkwd, outer_sol.psi_backwards)
-    ax2.set_xlabel(r'Minor radial co-ordinate ($r/a$)')
-    ax2.set_ylabel(r'Perturbed flux $(a^2 B_{\phi 0})$')
+    ax2.set_xlabel(r"Minor radial co-ordinate ($r/a$)")
+    ax2.set_ylabel(r"Perturbed flux $(a^2 B_{\phi 0})$")
 
     fig.tight_layout()
+
 
 def plot_outer_region_solution(params: TearingModeParameters):
     tm = solve_system(params)
-    
+
     fig, axs = plt.subplots(2)
     ax, ax_dpsi_dr = axs
-    
-    max_psi = max(
-        np.max(tm.psi_forwards), 
-        np.max(tm.psi_backwards)
-    )
-    ax.plot(tm.r_range_fwd, tm.psi_forwards/max_psi)
-    ax.plot(tm.r_range_bkwd, tm.psi_backwards/max_psi)
-    
+
+    max_psi = max(np.max(tm.psi_forwards), np.max(tm.psi_backwards))
+    ax.plot(tm.r_range_fwd, tm.psi_forwards / max_psi)
+    ax.plot(tm.r_range_bkwd, tm.psi_backwards / max_psi)
+
     ax.set_ylabel("Normalised perturbed flux")
-    
+
     ax_dpsi_dr.plot(tm.r_range_fwd, tm.dpsi_dr_forwards)
     ax_dpsi_dr.plot(tm.r_range_bkwd, tm.dpsi_dr_backwards)
-    
+
     ax_dpsi_dr.set_ylabel("$\partial \delta\psi^{(1)}/\partial r$")
-    
+
     ax_dpsi_dr.set_xlabel("Normalised minor radial co-ordinate (a)")
-    
+
     fname = f"jorek_model_growth_(m,n)=({params.poloidal_mode_number},{params.toroidal_mode_number})"
-    #savefig(fname)
-    
-def plot_delta_prime(outer_sol: OuterRegionSolution, 
-                     time_dep_sol: TimeDependentSolution):
+    # savefig(fname)
+
+
+def plot_delta_prime(
+    outer_sol: OuterRegionSolution, time_dep_sol: TimeDependentSolution
+):
     r_s = outer_sol.r_s
-    dps = r_s*np.array(time_dep_sol.delta_primes)
+    dps = r_s * np.array(time_dep_sol.delta_primes)
     times = time_dep_sol.times
-    
+
     fig, ax = plt.subplots(1)
     ax.plot(times, dps)
-    
-    ax.set_xlabel('Normalised time (1/$\omega_A$)')
+
+    ax.set_xlabel("Normalised time (1/$\omega_A$)")
     ax.set_ylabel("$r_s\Delta'(w)$")
-    
+
     fig.tight_layout()
-    
-    
+
+
 def plot_input_profiles():
     psi_current_prof_filename = "../../jorek_tools/postproc/exprs_averaged_s00000.csv"
     q_prof_filename = "../../jorek_tools/postproc/qprofile_s00000.dat"
-    q_profile, j_profile = q_and_j_from_csv(
-        psi_current_prof_filename, q_prof_filename
-    )
+    q_profile, j_profile = q_and_j_from_csv(psi_current_prof_filename, q_prof_filename)
 
     fig, axs = plt.subplots(2)
 
@@ -117,76 +119,65 @@ def plot_input_profiles():
 
     rs, qs = zip(*q_profile)
     axq.plot(rs, qs)
-    #axq.set_xlabel('Minor radial coordinate (a)')
-    axq.set_ylabel('Safety factor')
+    # axq.set_xlabel('Minor radial coordinate (a)')
+    axq.set_ylabel("Safety factor")
 
     rs, js = zip(*j_profile)
     axj.plot(rs, js)
-    axj.set_xlabel('Minor radial coordinate (a)')
-    axj.set_ylabel('Current density')
+    axj.set_xlabel("Minor radial coordinate (a)")
+    axj.set_ylabel("Current density")
 
     plt.show()
+
 
 def ql_tm_vs_time():
     """
     Numerically solve the quasi-linear time-dependent tearing mode problem
     and plot the perturbed flux and layer width as functions of time.
     """
-    
+
     if len(sys.argv) < 3:
-        experiment_root = expanduser("~/csd3/jorek_data/intear_ntor3_cylinder/run_47608261/postproc")
-        
-        psi_current_prof_filename = join(
-            experiment_root, "exprs_averaged_s00000.csv"
+        experiment_root = expanduser(
+            "~/csd3/jorek_data/intear_ntor3_cylinder/run_47608261/postproc"
         )
+
+        psi_current_prof_filename = join(experiment_root, "exprs_averaged_s00000.csv")
         q_prof_filename = join(experiment_root, "qprofile_s00000.dat")
     else:
         psi_current_prof_filename = sys.argv[1]
         q_prof_filename = sys.argv[2]
-        
-    q_profile, j_profile = q_and_j_from_csv(
-        psi_current_prof_filename, q_prof_filename
-    )
-    
-    
-    #rq, q = zip(*q_profile)
-    #q = np.array(q)/q[0]
-    #rj, js = zip(*j_profile)  
-    #js = 10.0*np.array(js)
-    #j_profile = list(zip(rj, 40.0*np.array(js)))
-    #fig, ax = plt.subplots(3)
-    #ax[0].plot(rq, q)
-    #ax[1].plot(rj, js)
-    #ax[2].plot(rj, dj_dr_vals)
-    
 
-    
+    q_profile, j_profile = q_and_j_from_csv(psi_current_prof_filename, q_prof_filename)
+
+    # rq, q = zip(*q_profile)
+    # q = np.array(q)/q[0]
+    # rj, js = zip(*j_profile)
+    # js = 10.0*np.array(js)
+    # j_profile = list(zip(rj, 40.0*np.array(js)))
+    # fig, ax = plt.subplots(3)
+    # ax[0].plot(rq, q)
+    # ax[1].plot(rj, js)
+    # ax[2].plot(rj, dj_dr_vals)
+
     params = TearingModeParameters(
-        poloidal_mode_number = 2,
-        toroidal_mode_number = 1,
-        lundquist_number = 3.73e9,
-        initial_flux = 1.336e-12,
+        poloidal_mode_number=2,
+        toroidal_mode_number=1,
+        lundquist_number=1.119e10,
+        initial_flux=1.336e-12,
         B0=1.0,
         R0=40.0,
-        q_profile = q_profile,
-        j_profile = j_profile
+        q_profile=q_profile,
+        j_profile=j_profile,
     )
-    
+
     sol = solve_system(params)
 
-    t0 = jorek_to_alfven_time(
-        1.04e4, # This is the jorek time at which the simulation numerically stabilises
-        params.B0, params.R0
-    )
-    times = np.append(
-        np.linspace(t0, 1e6, 10000),
-        np.linspace(1e6, 8e6, 10000)
-    )
+    t0 = 4.2444e5  # This is the jorek time at which the simulation numerically stabilises
 
-    
-    ql_solution = solve_time_dependent_system(
-        params, times
-    )
+    times = np.linspace(t0, 2e9, 200000)
+    print(max(times))
+
+    ql_solution = solve_time_dependent_system(params, times)
     times = ql_solution.times
     psi_t = ql_solution.psi_t
     dpsi_t = ql_solution.dpsi_dt
@@ -194,49 +185,48 @@ def ql_tm_vs_time():
 
     print(times)
     print(ql_solution.psi_t)
-    #plt.plot(w_t)
-    #plt.show()
+    # plt.plot(w_t)
+    # plt.show()
 
-    #print("lgr: ", lin_growth_rate)
-    #print("Threshold: ", ql_threshold)
-    #print(psi_t)
+    # print("lgr: ", lin_growth_rate)
+    # print("Threshold: ", ql_threshold)
+    # print(psi_t)
 
-    fig, ax = plt.subplots(1, figsize=(4,3.5))
+    fig, ax = plt.subplots(1, figsize=(4, 3.5))
     ax2 = ax.twinx()
 
-
-    ax.plot(times, psi_t, label='Flux', color='black')
+    ax.plot(times, psi_t, label="Flux", color="black")
 
     ax.set_xlabel(r"Normalised time ($\bar{\omega}_A t$)")
     ax.set_ylabel(r"Normalised perturbed flux ($\delta \psi^{(1)}$)")
 
-    ax2.plot(times, w_t, label='Normalised island width', color='red')
+    ax2.plot(times, w_t, label="Normalised island width", color="red")
     ax2.set_ylabel(r"Normalised electrostatic modal width ($\hat{\delta}_{ql}$)")
-    ax2.yaxis.label.set_color('red')
+    ax2.yaxis.label.set_color("red")
 
-    ax.legend(prop={'size': 7}, loc='lower right')
+    ax.legend(prop={"size": 7}, loc="lower right")
 
     # ax.set_yscale('log')
     # ax2.set_yscale('log')
-    ax.set_xscale('log')
-    ax2.set_xscale('log')
-    ax.set_yscale('log')
-    ax2.set_yscale('log')
+    ax.set_xscale("log")
+    ax2.set_xscale("log")
+    ax.set_yscale("log")
+    ax2.set_yscale("log")
 
     fig.tight_layout()
-    #plt.show()
+    # plt.show()
 
     fname = f"jorek_model_(m,n)=({params.poloidal_mode_number},{params.toroidal_mode_number})"
     savefig(fname)
     sim_to_disk(fname, params, ql_solution)
-    
+
     plot_growth(times, dpsi_t, psi_t)
     plot_delta_prime(sol, ql_solution)
     plot_energy(params, ql_solution, sol)
-    
+
     plt.show()
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     ql_tm_vs_time()
-    #plot_input_profiles()
+    # plot_input_profiles()
