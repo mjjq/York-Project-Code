72176b324cd00af3aa1fb278a15c0e50d41073f1
Marcus Quantrill <marcus_quantrill@hotmail.com>
2024-11-15 10:28:07 +0000

    Update jorek_tools debug.sh

--- application/jorek_tools/debug/debug.sh
+++ application/jorek_tools/debug/debug.sh
@@ -1,1 +1,2 @@
-nice -n 20 mpirun -ppn 4 -np 1 ./jorek_model_debug < $1
+export OMP_NUM_THREADS=1
+srun --cpu-bind=cores -A ukaea-ap002-cpu -p icelake-himem --mem=8000 -N 1 -n 1 -t 00:10:00 mpirun -n 1 ./jorek_model < $1
