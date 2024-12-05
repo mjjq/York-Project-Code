5124ccb775cc22ef010f2ddad2054b2a42736d2e
Marcus Quantrill <marcus_quantrill@hotmail.com>
2024-12-05 13:29:51 +0000

    Add git information to build directory after making JOREK

--- application/jorek_tools/build/make_jorek.sh
+++ application/jorek_tools/build/make_jorek.sh
@@ -14,3 +14,6 @@ make clean && make -j 32 DEBUG=1
 mv jorek_model* ./build/jorek_model_debug
 
 ./util/config.sh > ./build/settings.txt
+
+git log -1 > ./build/git_info.txt
+git branch >> ./build/git_info.txt
