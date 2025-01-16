c2c5dd1e8a8675479f9791b3ed63d39879e190b6
Marcus Quantrill <marcus_quantrill@hotmail.com>
2025-01-16 14:41:21 +0000

    Update macroscopic vars extraction

--- application/jorek_tools/macroscopic_vars_analysis/extract_multiple.sh
+++ application/jorek_tools/macroscopic_vars_analysis/extract_multiple.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for d in $(find ./ -type d -name run_*); do (cd "$d" && $JOREK_UTIL/extract_live_data.sh magnetic_energies > magnetic_energies.dat); done
+for d in $(find ./ -type d -name "run_*"); do (cd "$d" && $JOREK_UTIL/extract_live_data.sh -si magnetic_energies > magnetic_energies.dat); done
