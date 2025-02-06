fc79f13bbb3aba6c7bd739f5a67981d2ddd702eb
Marcus Quantrill <marcus_quantrill@hotmail.com>
2025-02-06 11:41:22 +0000

    Fixed mapping between time step and simulation time. Create script to automatically extract and plot different modes vs time

--- application/jorek_tools/delta_psi_extraction/time.sh
+++ application/jorek_tools/delta_psi_extraction/time.sh
@@ -19,13 +19,21 @@ function extract_time_from_log() {
 	grep -r "t_now" $logfile | sed 's/After step //' | sed 's/(t_now= //' | sed 's/)://'
 }
 
+function extract_abs_step_from_log() {
+	grep -E "time step" $logfile | awk '{ printf("%05d %05d\n", $6, $7) }'
+}
+
+function abs_time_from_log() {
+	join <(extract_abs_step_from_log) <(extract_time_from_log) | awk '{ print $2 " " $3 }'
+}
+
 function extract_mu0_rho0_from_log() {
 	grep -r "mu0\*rho0" $logfile | awk 'NF>1{print $NF}'
 }
 
 
 function extract_time_to_si() {
-	extract_time_from_log | awk -v t=$(extract_mu0_rho0_from_log) '{print $1 " " t*$2}'
+	abs_time_from_log | awk -v t=$(extract_mu0_rho0_from_log) '{print $1 " " t*$2}'
 }
 
 extract_time_to_si
