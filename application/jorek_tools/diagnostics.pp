d4bf4de1e68f8738dc64eeb4017698f401ba82c7
Marcus Quantrill <marcus_quantrill@hotmail.com>
2025-04-22 12:34:33 +0100

    diagnostics postproc script now runs through all restart files. Remove V_phi and use default nmaxsteps

--- application/jorek_tools/diagnostics.pp
+++ application/jorek_tools/diagnostics.pp
@@ -4,14 +4,13 @@ si-units
 set verbose true
 set debug true
 
-for step 0 do
+for step 0 to 99999 do
 
 qprofile
 
-expressions Psi_N r_minor ne T_i T_e pres FFprime_loc zj currdens Btor V_phi vpar
+expressions Psi_N r_minor ne T_i T_e pres FFprime_loc zj currdens Btor vpar
 mark_coords 1
 set surfaces 200
-set nmaxsteps 6000
 average
 
 done
