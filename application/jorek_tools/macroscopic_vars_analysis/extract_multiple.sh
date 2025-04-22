a55dda110c3a805be6b645fb1fc15d2453142897
Marcus Quantrill <marcus_quantrill@hotmail.com>
2025-04-22 12:33:05 +0100

    Update macroscopic variable extraction and plotting for multiple runs

--- application/jorek_tools/macroscopic_vars_analysis/extract_multiple.sh
+++ application/jorek_tools/macroscopic_vars_analysis/extract_multiple.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 for d in $(find ./ -type d -name "run_*"); do (cd "$d" && $JOREK_UTIL/extract_live_data.sh -si magnetic_energies > magnetic_energies.dat); done
+for d in $(find ./ -type d -name "run_*"); do (cd "$d" && $JOREK_UTIL/extract_live_data.sh -si magnetic_growth_rates > magnetic_growth_rates.dat); done
--- application/jorek_tools/macroscopic_vars_analysis/plot_multiple.sh
+++ application/jorek_tools/macroscopic_vars_analysis/plot_multiple.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-python3 $JOREK_TOOLS/macroscopic_vars_analysis/plot_quantities.py -f $(cat useful_runs.txt | sed 's/$/magnetic_energies.dat/') -ci 2 -ys log -x0 0 -yl 'Magnetic energy [JOREK] units' -o output.png
+python3 $JOREK_TOOLS/macroscopic_vars_analysis/plot_quantities.py -f $(cat useful_runs.txt | sed 's/$/\/magnetic_energies.dat/') -yi 2 -ys log -x0 0 -xl "Time (s)" -yl 'Magnetic energy [JOREK] units' -l $(cat labels.txt) -o mag_energies.png
+python3 $JOREK_TOOLS/macroscopic_vars_analysis/plot_quantities.py -f $(cat useful_runs.txt | sed 's/$/\/magnetic_growth_rates.dat/') -yi 2 -x0 0 -xl "Time (s)" -yl "Magnetic growth rate (s$^{-1}$)" -l $(cat labels.txt) -o mag_growth_rates.png
