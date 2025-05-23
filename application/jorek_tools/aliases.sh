529ef49ef4fcec62873e3585eb731069212e990f
Marcus Quantrill <marcus_quantrill@hotmail.com>
2025-05-23 14:16:45 +0100

    Custom labels for energy/growth plots if specified in relevant aliases

--- application/jorek_tools/aliases.sh
+++ application/jorek_tools/aliases.sh
@@ -5,12 +5,18 @@ alias analysis-venv="source $PROJ_HOME/jorek_analysis/York-Project-Code/venv/bin
 alias plg="$JOREK_UTIL/plot_grids.sh"
 alias pld="$JOREK_UTIL/plot_live_data.sh"
 alias j2vtk="$JOREK_UTIL/convert2vtk.sh -j 32 ./jorek2vtk ./inmastu"
+alias j2vtkno0="$JOREK_UTIL/convert2vtk.sh -no0 -j 32 ./jorek2vtk ./inmastu"
 
 alias plq="python3 $JOREK_TOOLS/macroscopic_vars_analysis/plot_quantities.py"
 
 batchgrowth() {
         cat useful_runs.txt | parallel 'cd {}; $JOREK_UTIL/extract_live_data.sh -si magnetic_growth_rates > magnetic_growth_rates.dat'
 }
+
+batchenergies() {
+        cat useful_runs.txt | parallel 'cd {}; $JOREK_UTIL/extract_live_data.sh -si magnetic_energies > magnetic_energies.dat'
+}
+
 
 getbetas() {
         cat useful_runs.txt | xargs find | grep log | xargs grep -m 1 betap | awk '{printf "%.3f\n", $5}'
@@ -24,10 +30,24 @@ batchqlgrowth() {
 	cat useful_runs.txt | parallel -k 'cd {}/postproc; python3 -m experiments.jorek_growth_comparison.curvature_stabilisation 0.0 -si'
 }
 
+getlabels() {
+	labelsarg="-l $(cat useful_runs.txt)"
+	test -n "$(cat useful_runs_labels.txt)" && labelsarg="-l $(cat useful_runs_labels.txt)"
+	echo "$labelsarg"
+}
+
 batchplotgrowth() {
         batchgrowth
-        plq -f $(cat useful_runs.txt | xargs find | grep magnetic_growth) -yi 2 -xl "Time (ms)" -yl "Magnetic growth rate (1/s)" -l $(getbetas | sed s'/^/$\\beta_p=$/')
+	labelsarg="$(getlabels)"
+        plq -f $(cat useful_runs.txt | xargs find | grep magnetic_growth) -yi 2 -xl "Time (ms)" -yl "Magnetic growth rate (1/s)" $labelsarg
 }
+
+batchplotenergies() {
+        batchenergies
+	labelsarg="$(getlabels)"
+        plq -f $(cat useful_runs.txt | xargs find | grep magnetic_energies) -yi 2 -xl "Time (ms)" -yl "Normalised magnetic energy (arb)" $labelsarg -ys log
+}
+
 
 batchplotqprof() {
         plq -f $(cat useful_runs.txt | xargs find | grep qprofile.dat) -xl "$\psi_N$" -yl "Safety factor" -l $(getbetas | sed s'/^/$\\beta_p=$/')
