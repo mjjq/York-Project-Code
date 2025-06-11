1424c703d63c6ea5bb065a51a206f0dc83e121a3
Marcus Quantrill <marcus_quantrill@hotmail.com>
2025-06-11 16:19:33 +0100

    Can specify which modes to plot in plotter

--- application/jorek_tools/delta_psi_extraction/plot_first_nine_psin.sh
+++ application/jorek_tools/delta_psi_extraction/plot_first_nine_psin.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 rm temp.dat
-for i in $(seq 1 9)
+for i in $(seq 1 $2)
 do
 	$SCRIPT_DIR/dpsi_rs_time.sh $i 1 $1 >> temp.dat
 done
