e6d70fafdb4d1d50cd281e1781763ef0539ad561
Marcus Quantrill <marcus_quantrill@hotmail.com>
2025-07-31 18:14:34 +0100

    Update t2_dependence script. Now takes in magnetic energy plots. Still need to fix limits

--- application/experiments/jorek_growth_comparison/t2_dependence_jorek.py
+++ application/experiments/jorek_growth_comparison/t2_dependence_jorek.py
@@ -1,179 +1,136 @@
-import pandas as pd
-from matplotlib import pyplot as plt
 from scipy.interpolate import CloughTocher2DInterpolator, UnivariateSpline
-import os
 import numpy as np
-from typing import List, Tuple
+from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
-import imports
-from tearing_mode_solver.outer_region_solver import (
-    rational_surface, magnetic_shear
-)
-from tearing_mode_solver.delta_model_solver import nu, mode_width
-from tearing_mode_solver.helpers import (
-    classFromArgs, TimeDependentSolution, savefig, load_sim_from_disk
-)
-from tearing_mode_solver.outer_region_solver import island_width
-from tearing_mode_solver.algebraic_fitting import get_parab_coefs
-from jorek_tools.calc_jorek_growth import growth_rate, _name_time, _name_flux
-from jorek_tools.jorek_dat_to_array import q_and_j_from_csv
-from jorek_tools.psi_t_from_vtk import jorek_flux_at_q
+from tearing_mode_solver.helpers import savefig
 
-def check_model_t_dependence():
-    model_data_filename = "./output/15-05-2024_16:49_jorek_model_(m,n)=(2,1).zip"
 
-    params, sol = load_sim_from_disk(model_data_filename)
+def find_best_quad_fit_location(t_vals: np.array,
+                                delta_psi_vals: np.array) -> float:
+    """
+    If function is quadratic, it must satisfy the equation
+    dx/dt = A*sqrt(x)
 
-    times = sol.times
-    psi_t = sol.psi_t
-    dpsi_t = sol.dpsi_dt
-    w_t = sol.w_t
-    d2psi_dt2 = sol.d2psi_dt2
-    delta_primes = sol.delta_primes
+    Hence, this is true in the discrete form via
 
-    print(delta_primes)
+    dx_i/dt = A*sqrt(x_i)
+
+    and
+
+    dx_{i+1}/dt = A*sqrt(x_{i+1}), implying
+
+    (dx_{i+1}/dt) / (dx_i/dt) = sqrt(x_{i+1}/x_i)
+
+    Hence, find the value of x_i that minimises the error in the above.
+    This is what we do here
+    """
+    
+    dpsi_spline = UnivariateSpline(t_vals, delta_psi_vals)
+    dpsi_dt_spline = dpsi_spline.derivative()
+
+    dpsi_dt_vals = dpsi_dt_spline(t_vals)
+
+    dpsi_dt_ratio = np.array([
+        dpsi_dt_vals[i+1]/dpsi_dt_vals[i] 
+        for i in range(len(dpsi_dt_vals)-1)
+    ])
+
+    xi_ratio = np.array([
+        np.sqrt(delta_psi_vals[i+1]/delta_psi_vals[i])
+        for i in range(len(delta_psi_vals)-1)
+    ])
+
+    diff = np.abs(dpsi_dt_ratio - xi_ratio)
 
     fig, ax = plt.subplots(1)
+    ax.plot(t_vals[:-1], diff)
+    ax.set_yscale('log')
 
-    ax.plot(times, psi_t)
-    ax.set_xscale('log')
-
-    plt.show()
-   
-def quadratic(x, a, b, c):
-    return a*x**2 + b*x + c
-
-def ql_tm_vs_time():
-    """
-    Plot various numerically solved variables from a tearing mode solution and
-    island width as a function of time from .csv data.
-    """
-    model_data_filename = "./output/21-05-2024_13:52_jorek_model_(m,n)=(2,1).zip"
-    jorek_data_filename = "../../jorek_tools/postproc/psi_t_data.csv"
-    q_prof_filename = "../../jorek_tools/postproc/qprofile_s00000.dat"
-    psi_current_prof_filename = "../../jorek_tools/postproc/exprs_averaged_s00000.csv"
-    q_profile, j_profile = q_and_j_from_csv(
-        psi_current_prof_filename, q_prof_filename
-    )
+    return t_vals[np.argmin(diff)]
 
 
-    params, sol = load_sim_from_disk(model_data_filename)
-
-    times = sol.times
-    psi_t = sol.psi_t
-    dpsi_t = sol.dpsi_dt
-    w_t = sol.w_t
-    d2psi_dt2 = sol.d2psi_dt2
-    delta_primes = sol.delta_primes
+def quadratic(t, t_0, delta_psi_0, c): 
+    return ((t-t_0 + 2*c*delta_psi_0**0.5)/(2*c))**2
 
 
-    jorek_data = pd.read_csv(jorek_data_filename).fillna(0)
-    jorek_times, jorek_flux = jorek_flux_at_q(jorek_data, q_profile, 2/1)
-    
+def fit_quadratic(t_vals: np.array,
+                  delta_psi_vals: np.array):
+    t_vals = t_vals/np.max(t_vals)
+    t_0 = find_best_quad_fit_location(t_vals, delta_psi_vals)
+    print(t_0)
 
-    min_time = 0.0#1e4
-    max_time = 1e6
+    dpsi_0 = np.interp(t_0, t_vals, delta_psi_vals)
 
-    # Model flux
-    model_filt = ((times<max_time) & (times> min_time))
-    times = times[model_filt]
-    model_flux = psi_t[model_filt]
-    model_flux_func = UnivariateSpline(times, model_flux, s=0)
+    t_vals_filt = t_vals[(t_vals>=t_0)]
+    dpsi_vals_filt = delta_psi_vals[(t_vals>=t_0)]
 
-    # JOREK flux
-    jorek_filt = jorek_times>min_time
-    jorek_times = jorek_times[jorek_filt]
-    jorek_flux = jorek_flux[jorek_filt]
-    jorek_flux_func = UnivariateSpline(jorek_times, jorek_flux)
-
-    # Parabolic fitting
-    min_t2_time = 95000
-    max_t2_time = 97500
-    c_0, c_1, c_2 = get_parab_coefs(params, model_flux_func(min_t2_time))
-
-    t = np.linspace(min_t2_time, max_t2_time, 100)
-
-    fig, ax = plt.subplots(1, figsize=(5,4))
-
-    #ax.plot(times, model_flux, label='Model', color='black')
-    ax.plot(t, jorek_flux_func(t), label='JOREK data', color='black')
-
-    #log_times = np.logspace(4, np.log(max(times)), 100)
-    #ax.plot(log_times, 1.0/log_times, label='2/t dependence')
+    partial_quadratic = lambda t, c : quadratic(t, t_0, dpsi_0, c)
 
     popt, pcov = curve_fit(
-        quadratic, 
-        t, 
-        jorek_flux_func(t),
-        bounds=([0.0, 0.0, 0.0], [np.inf, np.inf, np.inf])
+        partial_quadratic, 
+        t_vals_filt, 
+        dpsi_vals_filt,
+        bounds=([0.0], [np.inf]),
+        p0=[1.0],
+        method='trf'
     )
-    a,b,c = popt
-    t2_results = quadratic(t, *popt)
+    c_fit = popt[0]
+    c_std = np.sqrt(np.diag(pcov))[0]
+    t2_results = partial_quadratic(t_vals, *popt)
+    t2_results_min = partial_quadratic(t_vals, c_fit-c_std)
+    t2_results_max = partial_quadratic(t_vals, c_fit+c_std)
+
+    print(c_fit)
     
+    fig, ax = plt.subplots(1, figsize=(4,3))
     ax.plot(
-        t, 
-        t2_results, 
-        label='$at^2 + bt + c$ \n'+f'a={a:.2e},\n b={b:.2e},\n c={c:.2e}', 
-        linestyle='--', color='red'
+        t_vals,
+        delta_psi_vals,
+        color='black',
+        label='JOREK'
     )
-    
-    ax.autoscale()
+    ax.plot(
+        t_vals, 
+        t2_results, 
+        linestyle='--', color='red',
+        label=r"$t^2$ fit: "f"C={c_fit:.3f}"
+    )
 
     #ax.set_xscale('log')
-    #ax.set_yscale('log')
+    ax.set_yscale('log')
 
-    ax.set_xlabel(r"Time ($1/\omega_A$)")
-    ax.set_ylabel(r"Flux at rational surface ($a^2 B_{\phi 0}$)")
+    dpsi_plot_min = 0.1*max(delta_psi_vals)
+    t_plot_min = t_vals[np.argmin(np.abs(dpsi_plot_min-delta_psi_vals))]
 
+    ax.set_xlim(left=t_plot_min, right=1.05)
+    ax.set_ylim(bottom=dpsi_plot_min, top=1.1*max(delta_psi_vals))
 
-
-    # ax.plot(
-    #     t, 
-    #     c_0*(t-min_t2_time)**2 +c_1*(t-min_t2_time) + c_2, 
-    #     color='green', linestyle='--', 
-    #     label=r'$f(t) = at^2 + bt + c$' + \
-    #         f"\n a={c_0:.2e},\n b={c_1:.2e},\n c={c_2:.2e}"
-    # )
-
+    ax.set_xlabel(r"Time ($t_{max}$)")
+    ax.set_ylabel(r"$\delta\psi$ (arb)")
+    ax.grid()
     ax.legend()
+
     fig.tight_layout()
-    
-    savefig("t2_fitting")
-    
-    # ax.set_xlim(left=40000, right=1e6)
-    # ax.set_ylim(bottom=1e-9, top=1e-2)
-    # fig.tight_layout()
-    
-    # savefig("flux_comparison_log_log_zoom")
-    
-    # ax.set_xscale('linear')
-    # ax.set_yscale('linear')
-    # fig.set_size_inches(6, 4.65, forward=True)
-    # ax.set_xlim(left=0, right=4e5)
-    # ax.set_ylim(bottom=0, top=0.00175)
-    # fig.tight_layout()
-    
-    # savefig("flux_comparison_lin_lin_early")
-    
-    # ax.set_xlim(left=0, right=1e6)
-    # ax.set_ylim(bottom=0, top=0.00175)
-    
-    # savefig("flux_comparison_lin_lin")
-    
-    
-    # ax.autoscale()
-    # ax.set_yscale("log")
-    # ax.set_xlim(left=0, right=5e5)
-    # savefig("flux_comparison_lin_log")
 
-
-    plt.show()
-
-
-
+    savefig("quadratic_fit")
 
 
 if __name__=='__main__':
-    ql_tm_vs_time()
-    #check_model_t_dependence()
+    import sys
+
+    fname = sys.argv[1]
+
+    data = np.loadtxt(fname, skiprows=1)
+
+    t_vals = data[:,0]
+    delta_psi_vals = data[:,2]**0.5
+
+    fit_quadratic(t_vals, delta_psi_vals)
+
+    fig, ax = plt.subplots(1)
+    ax.plot(t_vals, delta_psi_vals)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+    plt.show()
