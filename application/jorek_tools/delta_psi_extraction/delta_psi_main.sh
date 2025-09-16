73bce4802dce54a99ca52f3f29c4cf14459f1c64
Marcus Quantrill <marcus_quantrill@hotmail.com>
2025-09-16 17:26:24 +0100

    Fix bug in delta_psi_main

--- application/jorek_tools/delta_psi_extraction/delta_psi_main.sh
+++ application/jorek_tools/delta_psi_extraction/delta_psi_main.sh
@@ -27,7 +27,7 @@ function extract_delta_psi_all()
 
 function plot_delta_psi_time()
 {
-    python3 -m jorek_tools.delta_psi_extraction.plot_delta_psi_vs_time -f postproc/exprs_four2d_s0*abs*n001.dat -q postproc/qprofile_s00000.dat -t postproc/times.txt
+    python3 -m jorek_tools.delta_psi_extraction.plot_delta_psi_vs_time -f postproc/exprs_four2d_s*abs*n001.dat -q postproc/qprofile_s00000.dat -t postproc/times.txt
 }
 
 function plot_delta_psi_psin()
