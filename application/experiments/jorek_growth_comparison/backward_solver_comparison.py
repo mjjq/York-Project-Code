afdc236a863069c47f4e58c0eaeb58677030cf16
mjjq <marcus_quantrill@hotmail.com>
2025-09-18 15:44:54 +0100

    Add resistive layer width and linear solution to backward rutherford solver plot

--- application/experiments/jorek_growth_comparison/backward_solver_comparison.py
+++ application/experiments/jorek_growth_comparison/backward_solver_comparison.py
@@ -3,22 +3,37 @@ from matplotlib import pyplot as plt
 from argparse import ArgumentParser
 
 from tearing_mode_solver.backward_rutherford_solver import solve_time_dependent_system
+from tearing_mode_solver.linear_solver import solve_time_dependent_system as solve_linear
 from tearing_mode_solver.helpers import (
     TimeDependentSolution,
     savefig,
     load_sim_from_disk,
     TearingModeParameters
 )
+from tearing_mode_solver.outer_region_solver import layer_width, growth_rate
 
 
 def plot_against_backward_rutherford(params: TearingModeParameters,
                                      ql_sol: TimeDependentSolution):
-    t_range = np.linspace(0.0, ql_sol.times[-1], 100000)
+    t_range = np.linspace(0.0, ql_sol.times[-1], 20000)
     
     params.initial_flux = ql_sol.psi_t[-1]
     initial_time = ql_sol.times[-1]
     
     td_sol, outer_sol = solve_time_dependent_system(params, t_range)
+
+    params.initial_flux = ql_sol.psi_t[0]
+    exp_sol, outer_sol_2 = solve_linear(params, t_range)
+    print(exp_sol.psi_t)
+    print(exp_sol.times)
+    print(exp_sol.w_t)
+
+    filt = exp_sol.psi_t < max(ql_sol.psi_t)
+    exp_w_t = exp_sol.w_t[filt]
+    print(exp_w_t)
+    exp_times = exp_sol.times[filt]
+
+    resistive_layer_width = layer_width(params)
 
     fig, ax = plt.subplots(1, figsize=(5,4))
     
@@ -29,11 +44,24 @@ def plot_against_backward_rutherford(params: TearingModeParameters,
         label='Quasi-linear solution'
     )
     ax.plot(
+        exp_times,
+        exp_w_t,
+        color='orange',
+        linestyle='--',
+        label='Linear solution'
+    )
+    ax.plot(
         initial_time - td_sol.times, 
         td_sol.w_t, 
         color='red', 
         linestyle='--', 
         label='Rutherford'
+    )
+    ax.hlines(
+        resistive_layer_width, 
+        min(td_sol.times), 
+        max(td_sol.times),
+        label="Resistive layer width"
     )
     
 
