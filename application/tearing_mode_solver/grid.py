1e7872fa824feda1d2cef80604c64aa762fb24d1
Marcus Quantrill <marcus_quantrill@hotmail.com>
2025-09-22 17:26:23 +0100

    More gridding options

--- application/tearing_mode_solver/grid.py
+++ application/tearing_mode_solver/grid.py
@@ -14,6 +14,38 @@ def grid_gaussian_dist(r0: float,
 
     return gaussian_grid
 
+def grid_uniform_dist(r0: float,
+                      rs: float,
+                      thickness: float,
+                      resolution: float) -> np.array:
+    
+    grid = np.arange(
+        r0, 
+        rs+np.sign(r0-rs)*thickness, 
+        np.sign(rs-r0)*resolution
+    )
+    grid[-1] = rs + np.sign(r0-rs)*thickness
+
+    return grid
+
+def grid_uniform_split(rs: float,
+                       rs_thickness: float,
+                       resolution: float) -> np.array:
+    # Convert to number of points since linspace ensures
+    # we have points at 0.0 and 1.0
+    n_points = int(1.0/resolution)
+    
+    fullgrid = np.linspace(0.0, 1.0, n_points)
+
+
+    fwd_grid = fullgrid[fullgrid < rs-rs_thickness]
+    bkwd_grid = fullgrid[fullgrid > rs+rs_thickness][::-1]
+
+    fwd_grid[-1] = rs-rs_thickness
+    bkwd_grid[-1] = rs+rs_thickness
+
+    return fwd_grid, bkwd_grid
+
 if __name__=='__main__':
     grid_gauss_left = grid_gaussian_dist(
         0.0,
