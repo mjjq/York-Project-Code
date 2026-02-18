0338b4a508977e95a0bd3a8d230d84dcf94a70fa
Marcus Quantrill <marcus_quantrill@hotmail.com>
2026-02-18 09:34:54 +0000

    Update postproc scripts

--- application/jorek_tools/equilibrium_diagnostics/ffprime.pp
+++ application/jorek_tools/equilibrium_diagnostics/ffprime.pp
@@ -1,8 +1,8 @@
 namelist inmastu
 
-for step 500 do
-expressions Psi_N FFprime_loc
+for step 0 do
+expressions R FFprime_loc
 mark_coords 1
-set surfaces 200
-average
+set linepoints 200
+midplane
 done
---
+++ application/jorek_tools/equilibrium_diagnostics/jtor_midplane.pp
@@ -0,0 +1,8 @@
+namelist inmastu
+
+for step 3000 do
+expressions R Jtor
+mark_coords 1
+set surfaces 300
+average
+done
