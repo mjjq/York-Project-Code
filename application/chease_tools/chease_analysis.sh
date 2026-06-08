85f0eb654950e1f04e31e79a10bb481bd9e7d1e8
Marcus Quantrill <mjjq500@york.ac.uk>
2026-06-08 19:11:03 +0200

    Add function to print values of cols at chosen index

--- application/chease_tools/chease_analysis.sh
+++ application/chease_tools/chease_analysis.sh
@@ -4,3 +4,11 @@ cols(){
 	cols_path=$1
 	head -n 1 $cols_path | tr -s ' ' '\n' | tail -n +2 | awk '{print NR-1 " " $0}'
 }
+
+tp() {
+	awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?RS:FS),a[i][j]}' "${1+FS=$1}";
+}
+
+cols_row(){
+	((head -n 1 $1 | sed 's/#//') && awk -v var="$2" '{if(NR==var) print $0}' $1) | tp
+}
