f87c70528a619c29e37bb6bc52ddf107c5892d59
Marcus Quantrill <mjjq500@york.ac.uk>
2026-08-05 20:23:23 +0200

    Add plot poincare function for jorek2_connection_fmhd

--- application/jorek_tools/poincare/plot_multiple.sh
+++ application/jorek_tools/poincare/plot_multiple.sh
@@ -17,8 +17,26 @@ __plq() {
 function gen_poincare() {
 	restart_file=$1
 	restart_no=$(restart_number $restart_file)
-	poincare_rz_filename=$(echo poinc_R-Z_$restart_no.txt)
-	poincare_rt_filename=$(echo poinc_rho-theta_$restart_no.txt)
+	
+	use_fmhd_flag=$2
+	use_fmhd=false
+	
+	nprocs_flag=$3
+	nprocs=1
+
+	extension="txt"
+
+	if [[ $use_fmhd_flag = "-f" ]]; then
+		use_fmhd=true
+		extension="vtk"
+	fi
+
+	if [[ $nprocs_flag ]]; then
+		nprocs=$nprocs_flag
+	fi
+
+	poincare_rz_filename=$(echo poinc_R-Z_$restart_no.$extension)
+	poincare_rt_filename=$(echo poinc_rho-theta_$restart_no.$extension)
 
 	if [ ! -f $poincare_rz_filename ]; then
 		tmp_folder=tmp_$restart_no
@@ -28,9 +46,16 @@ function gen_poincare() {
 		rm jorek_restart.h5
 
 		cp ../$restart_file jorek_restart.h5
-		./jorek2_poincare < inmastu
-		mv poinc_R-Z.dat ../$poincare_rz_filename
-		mv poinc_rho-theta.dat ../$poincare_rt_filename
+		
+		if [[ $use_fmhd = false ]]; then
+			./jorek2_poincare < inmastu
+			mv poinc_R-Z.dat ../$poincare_rz_filename
+			mv poinc_rho-theta.dat ../$poincare_rt_filename
+		else
+			rm connection.vtk
+			mpirun -np $nprocs ./jorek2_connection_fmhd < inmastu
+			mv connection.vtk ../$poincare_rz_filename
+		fi
 
 		cd ../
 		rm -r $tmp_folder
@@ -63,4 +88,22 @@ function gen_poincare_parallel() {
 	fi
 
 	printf '%s\n' jorek[0-9]*.h5 | awk -v n="$every_nth_file" -v m="$modulo" 'NR % n == m' | xargs -t -P $1 -I {} bash -c 'gen_poincare "{}"'
+}
+
+function gen_poincare_parallel_fmhd() {
+	export -f gen_poincare
+	export -f restart_number
+	every_nth_file=$2
+	if [[ ! "$every_nth_file" ]]; then
+		every_nth_file=1
+	fi
+
+	modulo=1
+	if [[ "$every_nth_file" == 1 ]]; then
+		modulo=0
+	fi
+
+	for f in $(printf '%s\n' jorek[0-9]*.h5 | awk -v n="$every_nth_file" -v m="$modulo" 'NR % n == m'); do
+		gen_poincare $f -f $1
+	done
 }
