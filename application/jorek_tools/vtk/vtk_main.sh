168f11edc8a8c45dd14cac9432b741fe5bf7270b
Marcus Quantrill <mjjq500@york.ac.uk>
2026-08-12 17:23:22 +0200

    Add convenient function for extracting certain traces from vtk

--- application/jorek_tools/vtk/vtk_main.sh
+++ application/jorek_tools/vtk/vtk_main.sh
@@ -3,3 +3,9 @@
 function plotvtk(){
     python3 -m jorek_tools.vtk.plot_vtk "$@"
 }
+
+function plotvtk-pnt(){
+    plotvtk "$@" -v Pres -t "Pressure [JU]" -o pressure.png &
+    plotvtk "$@" -v Te_keV -t "\$T_e\$ (keV)" -o temperature.png &
+    plotvtk "$@" -v ne20_m-3 -t "\$n_e\$ (10\$^{20}\$/m\$^3\$)" -o density.png &
+}
