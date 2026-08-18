4f6d3fd099db7c3392efc4b570a7addfa4889d15
Marcus Quantrill <mjjq500@york.ac.uk>
2026-08-18 19:08:19 +0200

    Add heat source term to zkpar pp script

--- application/jorek_tools/equilibrium_diagnostics/zkpar.pp
+++ application/jorek_tools/equilibrium_diagnostics/zkpar.pp
@@ -6,7 +6,7 @@ si-units
 set only_itor 0
 set rad_range_max 0.9495
 for step 0 do
-expressions Psi_N zkpar_T zkprof eta_T eta_num_T visco_T visco_num_T T
+expressions Psi_N zkpar_T zkprof eta_T eta_num_T visco_T visco_num_T T heat_source particle_source
 mark_coords 1
 set surfaces 200
 average
